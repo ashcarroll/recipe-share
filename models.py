6179f7fd561abcc2859ee1e6a1e9af62878cd590
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-11-22 18:55:52 +0000

    Added primary keys to all classes and defined attributes in User and Recipe class

--- models.py
+++ models.py
@@ -10,21 +10,24 @@ class User(UserMixin, db.Model):
     _tablename_ = 'users'
     user_id = db.mapped_column(db.Integer, primary_key=True)
     username = db.mapped_column(db.String(50), unique=True)
-    is_chef = ...
+    is_chef = db.mapped_column(db.Boolean, default=False)
 
 class Recipe(db.Model):
     _tablename_ = 'recipes'
-    title = ...
-    ingredients = ...
-    method = ...
-    total_time = ...
+    recipe_id = db.mapped_column(db.Integer, primary_key=True)
+    recipe_name = db.mapped_column(db.String(500))
+    ingredients = db.mapped_column(db.String(1000))
+    method = db.mapped_column(db.String(5000))
+    total_time = db.mapped_column(db.Integer)
 
 
 class Favorite(db.Model):
     _tablename_ = 'favorites'
+    favorite_id = db.mapped_column(db.Integer, primary_key=True)
     user_id = ...
     recipe_id = ...
 
 class Cuisine(db.Model):
     _tablename_ = 'cuisines'
-    name = ...
+    cuisine_id = db.mapped_column(db.Integer, primary_key=True)
+    cuisine_name = ...
