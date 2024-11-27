42fb633e2437387cc1842ee13a0dd53abca43bd9
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-11-27 19:06:56 +0000

    added and set up log in management to app.py

--- app.py
+++ app.py
@@ -8,14 +8,20 @@ from models import User, Recipe, Favorite, Cuisine, db
 app = Flask(__name__)
 app.config.from_object('config')
 
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+
 with app.app_context():
     db.init_app(app)
     db.create_all()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
