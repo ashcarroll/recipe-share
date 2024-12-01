880a305eed7fe5d69874dbe2e9b04042b2bfc186
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-12-01 11:55:30 +0000

    Added render URL

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+SECRET_KEY =os.getenv('SECRET_KEY', 'default_secret_key')
+
+
+# SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
+
+# DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
